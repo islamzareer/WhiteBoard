6d545af6c474626532b4bc4917d87e4455496852
islam zareer <187062@ppu.edu.ps>
2022-12-20 15:49:00 +0200

    edit icons

--- lib/board.dart
+++ lib/board.dart
@@ -17,6 +17,7 @@ class _BoardState extends State<Board> {
   late Color pickerColor = Colors.blue;
   double strokeWidth = 3.0;
   bool showBottomList = false;
+  bool expandTool = false;
 
   late WhiteBoardController _controller;
   @override
@@ -82,77 +83,97 @@ class _BoardState extends State<Board> {
                         mainAxisAlignment: MainAxisAlignment.end,
                         crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
+                          Visibility(
+                            visible: expandTool,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                CustomFloatingActionButton(
+                                  tag: 0,
+                                  function: () {
+                                    setState(() {
+                                      selectedColor = Colors.black;
+                                    });
+                                  },
+                                  icon: const Icon(FontAwesomeIcons.pen),
+                                ),
+                                const SizedBox(height: 2),
+                                CustomFloatingActionButton(
+                                  tag: 1,
+                                  function: _controller.undo,
+                                  icon: const Icon(
+                                      FontAwesomeIcons.arrowRotateLeft),
+                                ),
+                                const SizedBox(height: 2),
+                                CustomFloatingActionButton(
+                                  tag: 2,
+                                  function: _controller.redo,
+                                  icon: const Icon(
+                                      FontAwesomeIcons.arrowRotateRight),
+                                ),
+                                const SizedBox(height: 2),
+                                CustomFloatingActionButton(
+                                  tag: 3,
+                                  function: () {
+                                    setState(() {
+                                      selectedColor = Colors.white;
+                                    });
+                                  },
+                                  icon: const Icon(FontAwesomeIcons.eraser),
+                                ),
+                                const SizedBox(height: 2),
+                                Row(
+                                  children: [
+                                    Visibility(
+                                      visible: showBottomList,
+                                      child: Slider(
+                                          value: strokeWidth,
+                                          max: 50.0,
+                                          min: 0.0,
+                                          onChanged: (val) {
+                                            setState(() {
+                                              strokeWidth = val;
+                                            });
+                                          }),
+                                    ),
+                                    CustomFloatingActionButton(
+                                      tag: 4,
+                                      function: () {
+                                        setState(() {
+                                          showBottomList = !showBottomList;
+                                        });
+                                      },
+                                      icon: const Icon(FontAwesomeIcons
+                                          .upRightAndDownLeftFromCenter),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 2),
+                                CustomFloatingActionButton(
+                                  tag: 5,
+                                  function: () {
+                                    showColorPacker(context);
+                                  },
+                                  icon: const Icon(FontAwesomeIcons.palette),
+                                ),
+                                const SizedBox(height: 2),
+                                CustomFloatingActionButton(
+                                  tag: 6,
+                                  function: _controller.clear,
+                                  icon: const Icon(FontAwesomeIcons.trash),
+                                ),
+                              ],
+                            ),
+                          ),
                           CustomFloatingActionButton(
-                            tag: 0,
+                            tag: 7,
                             function: () {
                               setState(() {
-                                selectedColor = Colors.black;
+                                expandTool = !expandTool;
                               });
                             },
-                            icon: const Icon(FontAwesomeIcons.pen),
-                          ),
-                          const SizedBox(height: 2),
-                          CustomFloatingActionButton(
-                            tag: 1,
-                            function: _controller.undo,
-                            icon: const Icon(FontAwesomeIcons.arrowRotateLeft),
-                          ),
-                          const SizedBox(height: 2),
-                          CustomFloatingActionButton(
-                            tag: 2,
-                            function: _controller.redo,
-                            icon: const Icon(FontAwesomeIcons.arrowRotateRight),
-                          ),
-                          const SizedBox(height: 2),
-                          CustomFloatingActionButton(
-                            tag: 3,
-                            function: () {
-                              setState(() {
-                                selectedColor = Colors.white;
-                              });
-                            },
-                            icon: const Icon(FontAwesomeIcons.eraser),
-                          ),
-                          const SizedBox(height: 2),
-                          Row(
-                            children: [
-                              Visibility(
-                                visible: showBottomList,
-                                child: Slider(
-                                    value: strokeWidth,
-                                    max: 50.0,
-                                    min: 0.0,
-                                    onChanged: (val) {
-                                      setState(() {
-                                        strokeWidth = val;
-                                      });
-                                    }),
-                              ),
-                              CustomFloatingActionButton(
-                                tag: 4,
-                                function: () {
-                                  setState(() {
-                                    showBottomList = !showBottomList;
-                                  });
-                                },
-                                icon: const Icon(FontAwesomeIcons
-                                    .upRightAndDownLeftFromCenter),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 2),
-                          CustomFloatingActionButton(
-                            tag: 5,
-                            function: () {
-                              showColorPacker(context);
-                            },
-                            icon: const Icon(FontAwesomeIcons.palette),
-                          ),
-                          const SizedBox(height: 2),
-                          CustomFloatingActionButton(
-                            tag: 6,
-                            function: _controller.clear,
-                            icon: const Icon(FontAwesomeIcons.trash),
+                            icon: const Icon(FontAwesomeIcons.penToSquare),
                           ),
                         ],
                       ),
