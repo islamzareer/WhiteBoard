9dc9bc3c266e91deeb8157824604564bd7d5d93d
islam zareer <187062@ppu.edu.ps>
2022-12-20 15:56:06 +0200

    edit folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:white_board/splash_screen.dart';
+import 'package:white_board/screens/splash_screen.dart';
 void main() {
   runApp(const MyApp());
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:white_board/board.dart';
+import 'package:white_board/screens/board.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
